42560a3f76596937feb7137e63a1b838d28c69b5
GildedSeraphim <nathanishaaz@gmail.com>
2025-08-02 01:25:02 -0500

    progress bar

--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,21 @@
 const std = @import("std");
+const Progress = std.Progress;
+
+const width = 256;
+const height = 256;
 
 pub fn main() !void {
     var wbuf: [4096]u8 = undefined;
     var file_writer = std.fs.File.stdout().writer(&wbuf);
     const out = &file_writer.interface;
 
-    const width = 256;
-    const height = 256;
+    var pbuf: [1024]u8 = undefined;
+    const pr = Progress.start(.{
+        .draw_buffer = &pbuf,
+        .estimated_total_items = width * height,
+        .root_name = "raytracing",
+    });
+    defer pr.end();
 
     try out.print("P3\n{d} {d}\n255\n", .{ width, height });
 
